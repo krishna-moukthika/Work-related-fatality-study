b3715f6ae349206db9470b5888710a6048c5bbca
krishna-moukthika <krishnamoukthika.rentala@gmail.com>
2023-07-12 22:47:34 +0530

    Create task2_1.sql

---
+++ task2_1.sql
@@ -0,0 +1,2 @@
+--What is the number of reported incidents? The task is to calculate the total number of incidents reported in the given dataset.
+SELECT COUNT(id) AS id_count FROM fatalities_cleaned;
